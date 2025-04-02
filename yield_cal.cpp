bc52811c3d37bd2f037728896eb56dbe25b44606
Virender Ranga <40511557+RangaVirender@users.noreply.github.com>
2025-04-02 17:37:33 +0530

    Add files via upload

--- yield_cal.cpp
+++ yield_cal.cpp
@@ -12,45 +12,52 @@ class MyMainFrame : public TGMainFrame
     TGLabel      *proj_charge_state_label;
     TGLabel      *total_charge_incident_label;
     TGLabel      *total_no_of_proj_label;
-    TGLabel      *target_mass_label;
+    TGLabel      *target_massNo_label;
     TGLabel      *target_density_label;
+    TGLabel      *total_no_of_target_nuclei_label;
     TGLabel      *det_radius_label;
     TGLabel      *det_dis_label;
     TGLabel      *det_eff_label;
+    TGLabel      *det_solid_angle_label;
     TGLabel      *rxn_crxn_label;
+    TGLabel      *yield_label;
+
     
     TGNumberEntry *proj_charge_state_entry;
     TGNumberEntry *total_charge_incident_entry;
-    TGNumberEntry *target_mass_entry;
+    TGNumberEntry *target_massNo_entry;
     TGNumberEntry *target_density_entry;
     TGNumberEntry *det_radius_entry;
     TGNumberEntry *det_dis_entry;
     TGNumberEntry *det_eff_entry;
     TGNumberEntry *rxn_crxn_entry;
     
-    TGString total_no_of_proj_string;
+    //TGString total_no_of_proj_string;
 
-    int total_charge_incident_int;
+    int    total_charge_incident_int;
     double total_no_of_proj_double;
-    int proj_charge_state_int;
-    
+    int    proj_charge_state_int;
+    int    target_massNo_int;
+    double target_density_double;
+    double total_no_of_target_nuclei_double;
+    double det_radius_double;
+    double det_dis_double;
+    double det_solid_angle_double;
+    double det_eff_double;
+    double rxn_crxn_double;
+    double yield_double;
+
     public:
     MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h);
     virtual ~MyMainFrame();
     void cal_yield_button_clicked();
     void update_text_output();  
     void exit_button_clicked();
-    void OnValueSet(); 
+    void OnValueSet();
+    TGString to_TGString(double); 
     virtual void CloseWindow(); 
     
 };
-
-// Callback function when value changes
-//void MyMainFrame::OnValueSet()
-//{   
- //   proj_integrated_current_int = proj_integrated_current_entry->GetIntNumber();
- //   std::cout << "proj_integrated_current_int: " << proj_integrated_current_int << std::endl;
-//}
 
 
 MyMainFrame::MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h) : TGMainFrame(p, w, h) 
@@ -58,65 +65,85 @@ MyMainFrame::MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h) : TGMainFrame(p,
     grid_frame_1 = new TGGroupFrame(this, "Projectile parameters", kHorizontalFrame);
    
     proj_charge_state_label = new TGLabel(grid_frame_1, "Projectile charge state");
+    
+    Pixel_t lightgray_color;
+    gClient->GetColorByName("lightgray",lightgray_color);
+    proj_charge_state_label->ChangeBackground(lightgray_color);
+    
+    proj_charge_state_label->Resize(100, 25);
     grid_frame_1->AddFrame(proj_charge_state_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
+    
     proj_charge_state_entry = new TGNumberEntry(grid_frame_1, 1, 2, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
     proj_charge_state_entry->SetLimits(TGNumberFormat::kNELLimitMin, 1);
+    proj_charge_state_entry->Resize(100, 25);
     grid_frame_1->AddFrame(proj_charge_state_entry, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
     total_charge_incident_label = new TGLabel(grid_frame_1, "Total charge incident (nano Coulomb)");
+    total_charge_incident_label->Resize(100, 25);
     grid_frame_1->AddFrame(total_charge_incident_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
-    total_charge_incident_entry = new TGNumberEntry(grid_frame_1, 1, 8, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
+    total_charge_incident_entry = new TGNumberEntry(grid_frame_1, 10000, 8, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
     total_charge_incident_entry->SetLimits(TGNumberFormat::kNELLimitMin, 1);
-    // Connect the value change signal to the callback function
-   // proj_integrated_current_entry->Connect("ValueSet()","MyMainFrame",this, "OnValueSet()");
-
+    total_charge_incident_entry->Resize(100, 25);
     grid_frame_1->AddFrame(total_charge_incident_entry, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
-    total_no_of_proj_label = new TGLabel(grid_frame_1, "No of projectiles: ");
+    total_no_of_proj_label = new TGLabel(grid_frame_1, "Total no of projectiles:.................. ");
     grid_frame_1->AddFrame(total_no_of_proj_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
     grid_frame_2 = new TGGroupFrame(this, "Target parameters", kHorizontalFrame);
     
-    target_mass_label = new TGLabel(grid_frame_2, "Target Mass no");
-    grid_frame_2->AddFrame(target_mass_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
-    target_mass_entry = new TGNumberEntry(grid_frame_2, 1, 2, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
-    target_mass_entry->SetLimits(TGNumberFormat::kNELLimitMin, 1);
-    grid_frame_2->AddFrame(target_mass_entry, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
+    target_massNo_label = new TGLabel(grid_frame_2, "Target Mass no");
+    grid_frame_2->AddFrame(target_massNo_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
+    target_massNo_entry = new TGNumberEntry(grid_frame_2, 16, 2, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
+    target_massNo_entry->SetLimits(TGNumberFormat::kNELLimitMin, 1);
+    target_massNo_entry->Resize(100, 25);
+    grid_frame_2->AddFrame(target_massNo_entry, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
-    target_density_label = new TGLabel(grid_frame_2, "Target density");
+    target_density_label = new TGLabel(grid_frame_2, "Target density (ug/cm2)");
     grid_frame_2->AddFrame(target_density_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
-    target_density_entry = new TGNumberEntry(grid_frame_2, 1, 2, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
-    target_density_entry->SetLimits(TGNumberFormat::kNELLimitMin, 1);
+    target_density_entry = new TGNumberEntry(grid_frame_2, 2200, 2, -1, TGNumberFormat::kNESReal);//default value, max digits, ID 
+    target_density_entry->SetLimits(TGNumberFormat::kNELLimitMinMax, 0.0, 1e7);
+    target_density_entry->Resize(100, 25);
     grid_frame_2->AddFrame(target_density_entry, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
+    total_no_of_target_nuclei_label = new TGLabel(grid_frame_2, "Total no of target nuclei:................ ");
+    grid_frame_2->AddFrame(total_no_of_target_nuclei_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
+    
+
     grid_frame_3 = new TGGroupFrame(this, "Detector geometry parameters", kHorizontalFrame);
     
-    det_radius_label = new TGLabel(grid_frame_3, "Detector Radius");
+    det_radius_label = new TGLabel(grid_frame_3, "Detector Radius (cm)");
     grid_frame_3->AddFrame(det_radius_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
-    det_radius_entry = new TGNumberEntry(grid_frame_3, 1, 2, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
-    det_radius_entry->SetLimits(TGNumberFormat::kNELLimitMin, 1);
+    det_radius_entry = new TGNumberEntry(grid_frame_3, 4.44, 2, -1, TGNumberFormat::kNESReal);//default value, max digits, ID 
+    det_radius_entry->SetLimits(TGNumberFormat::kNELLimitMinMax, 0.0, 1e3);
+    det_radius_entry->Resize(100, 25);
     grid_frame_3->AddFrame(det_radius_entry, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
-    det_dis_label = new TGLabel(grid_frame_3, "Detector distance");
+    det_dis_label = new TGLabel(grid_frame_3, "Detector distance (cm)");
     grid_frame_3->AddFrame(det_dis_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
-    det_dis_entry = new TGNumberEntry(grid_frame_3, 1, 2, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
-    det_dis_entry->SetLimits(TGNumberFormat::kNELLimitMin, 1);
+    det_dis_entry = new TGNumberEntry(grid_frame_3, 20, 2, -1, TGNumberFormat::kNESReal);//default value, max digits, ID 
+    det_dis_entry->SetLimits(TGNumberFormat::kNELLimitMinMax, 0.0, 1e4);
+    det_dis_entry->Resize(100, 25);
     grid_frame_3->AddFrame(det_dis_entry, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
-    det_eff_label = new TGLabel(grid_frame_3, "Detector efficiency");
+    det_eff_label = new TGLabel(grid_frame_3, "Detector efficiency(%)");
     grid_frame_3->AddFrame(det_eff_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
-    det_eff_entry = new TGNumberEntry(grid_frame_3, 1, 2, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
-    det_eff_entry->SetLimits(TGNumberFormat::kNELLimitMin, 1);
+    det_eff_entry = new TGNumberEntry(grid_frame_3, 15, 3, -1, TGNumberFormat::kNESReal);//default value, max digits, ID 
+    det_eff_entry->SetLimits(TGNumberFormat::kNELLimitMinMax, 0.0, 100.0);
+    det_eff_entry->Resize(100, 25);
     grid_frame_3->AddFrame(det_eff_entry, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
+    
+    det_solid_angle_label = new TGLabel(grid_frame_3, "Detector solid angle(Sr)");
+    grid_frame_3->AddFrame(det_solid_angle_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
 
     grid_frame_4 = new TGGroupFrame(this, "Calculate", kHorizontalFrame);
 
     rxn_crxn_label = new TGLabel(grid_frame_4, "Reaction cross-section(milli barn)");
     grid_frame_4->AddFrame(rxn_crxn_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
-    rxn_crxn_entry = new TGNumberEntry(grid_frame_4, 1, 2, -1, TGNumberFormat::kNESInteger);//default value, max digits, ID 
-    rxn_crxn_entry->SetLimits(TGNumberFormat::kNELLimitMin, 1);
+    rxn_crxn_entry = new TGNumberEntry(grid_frame_4, 100, 2, -1, TGNumberFormat::kNESReal);//default value, max digits, ID 
+    rxn_crxn_entry->SetLimits(TGNumberFormat::kNELLimitMinMax, 0.0, 1e6);
+    rxn_crxn_entry->Resize(100, 25);
     grid_frame_4->AddFrame(rxn_crxn_entry, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
     label = new TGLabel(grid_frame_4, "........................");
@@ -127,10 +154,14 @@ MyMainFrame::MyMainFrame(const TGWindow *p, UInt_t w, UInt_t h) : TGMainFrame(p,
     cal_yield_button->Connect("Clicked()", "MyMainFrame", this, "cal_yield_button_clicked()"); // Connect to click handler
     grid_frame_4->AddFrame(cal_yield_button, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
 
+    yield_label = new TGLabel(grid_frame_4, "Yield(per sec):.........................");
+    grid_frame_4->AddFrame(yield_label, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
+    
     exit_button = new TGTextButton(grid_frame_4, "Exit");
     exit_button->Connect("Clicked()", "MyMainFrame", this, "exit_button_clicked()"); // Connect to exit handler
     grid_frame_4->AddFrame(exit_button, new TGLayoutHints(kLHintsCenterX, 5, 5, 5, 0));
     
+
     AddFrame(grid_frame_1, new TGLayoutHints(kLHintsExpandX, 5, 5, 5, 5));
     AddFrame(grid_frame_2, new TGLayoutHints(kLHintsExpandX, 5, 5, 5, 5));
     AddFrame(grid_frame_3, new TGLayoutHints(kLHintsExpandX, 5, 5, 5, 5));
@@ -153,29 +184,45 @@ MyMainFrame::~MyMainFrame() {
     Cleanup();
 }
 
+TGString MyMainFrame::to_TGString(double number_double)
+{
+    // Convert double to scientific notation string
+    std::stringstream ss;
+    ss << std::scientific << std::setprecision(2) << number_double;
+    return TGString(ss.str().c_str());
+    
+}
+
 void MyMainFrame::cal_yield_button_clicked()
 {
     
-   // proj_integrated_current_entry->Modified();
-
+   
     label->SetText("...........Working..........");
     
     total_charge_incident_int = total_charge_incident_entry->GetNumber();
-    //cout << "total_charge_incident_int: " << total_charge_incident_int << endl;
-    
-    proj_charge_state_int = proj_charge_state_entry->GetNumber();
-    //cout << "proj_charge_state_int: " << proj_charge_state_int << endl; 
+        proj_charge_state_int =     proj_charge_state_entry->GetNumber();
     
     total_no_of_proj_double = proj_charge_state_int * total_charge_incident_int * 1e-9/(1.6e-19);
-    //cout << "total_no_of_proj_double: " << total_no_of_proj_double << endl;
-
-    // Convert double to scientific notation string
-    std::stringstream ss;
-    ss << std::scientific << std::setprecision(2) << total_no_of_proj_double;
-    total_no_of_proj_string = TGString(ss.str().c_str());
     
-    //cout << "total_no_of_proj_string: " << total_no_of_proj_string << endl;
-    total_no_of_proj_label->SetText(total_no_of_proj_string);
+        target_massNo_int =    target_massNo_entry->GetNumber();
+    target_density_double = target_density_entry->GetNumber();
+
+    total_no_of_target_nuclei_double = 6.022e23*1e-6*target_density_double/target_massNo_int;
+
+    det_radius_double = det_radius_entry->GetNumber();
+       det_dis_double =    det_dis_entry->GetNumber();
+             
+    det_solid_angle_double = 2.0*M_PI*(1.0 - det_dis_double/sqrt(pow(det_dis_double,2.0) + pow(det_radius_double, 2.0) ) );
+    
+    rxn_crxn_double = rxn_crxn_entry->GetNumber()*1e-27;//mb to per cm2
+    det_eff_double  = det_eff_entry->GetNumber()/100.0;
+
+    yield_double = rxn_crxn_double*total_no_of_proj_double*total_no_of_target_nuclei_double*det_solid_angle_double*det_eff_double/(4*M_PI);
+
+             total_no_of_proj_label->SetText("Total no of projectiles: "  + to_TGString(total_no_of_proj_double));
+    total_no_of_target_nuclei_label->SetText("Total no of target nuclei: "+ to_TGString(total_no_of_target_nuclei_double));
+              det_solid_angle_label->SetText("Detector solid angle (Sr): "+ to_TGString(det_solid_angle_double));
+                        yield_label->SetText("Yield(for charge incident): "          +to_TGString(yield_double));
     label->SetText("Calculations Complete");
     return;
 }
